44eadf4fee960d94e3a225387d7e78eb55a6a6ae
Igor Bressan <iarcanjo@users.noreply.github.com>
2016-10-12 15:52:43 -0300

    Update HelloWorldJava
    Alterando classe via github web, para realizar testes na configuração do Fetch na ide.

--- src/hello/HelloWorldJava.java
+++ src/hello/HelloWorldJava.java
@@ -3,7 +3,7 @@ package hello;
 public class HelloWorldJava {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World Java!");
+		System.out.println("Hello World Java...");
 	}
 
 }
