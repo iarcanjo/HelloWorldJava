081a36770a17cb81e6fd087e8972ee984d2e243d
iarcanjo <i.arcanjo@gmail.com>
2016-10-22 20:28:58 -0200

    Adicionando exercicio de sockets e solução do sistema de chat

--- src/sockets/Cliente.java
+++ src/sockets/Cliente.java
@@ -9,17 +9,35 @@ import java.util.Scanner;
 public class Cliente {
 
 	public static void main(String[] args) throws UnknownHostException, IOException {
-		Socket cliente = new Socket("127.0.0.1", 12345);
+		new Cliente("127.0.0.1", 12345).executa();
+
+	}
+
+	private String host;
+	private int porta;
+
+	public Cliente(String host, int porta) {
+		this.host = host;
+		this.porta = porta;
+	}
+
+	public void executa() throws UnknownHostException, IOException {
+		Socket cliente = new Socket(this.host, this.porta);
 		System.out.println("O cliente se conectou ao servidor!");
 
-		Scanner scan = new Scanner(System.in);
-		PrintStream saida = new PrintStream(cliente.getOutputStream());
+		// thread para receber msgs do servidor
+		Recebedor r = new Recebedor(cliente.getInputStream());
+		new Thread(r).start();
 
-		while (scan.hasNextLine()) {
-			saida.println(scan.nextLine());
+		// lê msgs do teclado e manda para o servidor
+		Scanner teclado = new Scanner(System.in);
+		PrintStream saida = new PrintStream(cliente.getOutputStream());
+		while (teclado.hasNextLine()) {
+			saida.println(teclado.nextLine());
 		}
+
 		saida.close();
-		scan.close();
+		teclado.close();
 		cliente.close();
 	}
 }
